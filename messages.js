c21591f134619f5cc718a4ddf5ce10df494bf1de
Cubeur-manchot <matthieu.aubert17@gmail.com>
2021-09-18 21:50:36 +0200

    Create function sendEmbedToChannel

--- messages.js
+++ messages.js
@@ -1,7 +1,12 @@
 "use strict";
 
+const Discord = require("discord.js");
+
 const sendMessageToChannel = (channel, message, options) =>
 	channel.send(message, options)
 		.catch(console.error);
 
-module.exports = {sendMessageToChannel};
+const sendEmbedToChannel = (channel, embedObject, attachments) =>
+	sendMessageToChannel(channel, {embed: new Discord.MessageEmbed(embedObject), files: attachments});
+
+module.exports = {sendMessageToChannel, sendEmbedToChannel};
