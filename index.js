f89a6abbc81825abc461c57ad044e9c63c49e9c3
Cubeur-manchot <matthieu.aubert17@gmail.com>
2023-06-05 15:49:38 +0200

    Upgrade Discord.js version to 14 : client & events

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ import Discord from "discord.js";
 
 import {onReady, onMessage} from "./eventHandler.js";
 
-const Mee7 = new Discord.Client();
+const Mee7 = new Discord.Client({
+	intents: [
+		Discord.GatewayIntentBits.Guilds,
+		Discord.GatewayIntentBits.GuildMessages,
+		Discord.GatewayIntentBits.MessageContent
+	],
+	partials: []
+});
 
-Mee7.on("ready", () => onReady(Mee7));
-Mee7.on("message", onMessage);
+Mee7.once(Discord.Events.ClientReady, onReady);
+Mee7.on(Discord.Events.MessageCreate, onMessage);
 
 Mee7.login(process.env.TOKEN)
 	.then(() => console.log("MEE7 is logged in !"))
