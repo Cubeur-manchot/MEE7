4e1ad0eabc70685680e49fbec87aa30fa33ef6c1
Cubeur-manchot <matthieu.aubert17@gmail.com>
2023-06-05 16:51:31 +0200

    Dynamically read Google credentials

--- eventHandler.js
+++ eventHandler.js
@@ -21,25 +21,22 @@ const onReady = Mee7 => {
 };
 
 const setupGoogleSheetsAPICredentials = () => {
-	fs.writeFile("credentials.json", JSON.stringify({
-		type: process.env.CREDENTIALS_type,
-		project_id: process.env.CREDENTIALS_project_id,
-		private_key_id: process.env.CREDENTIALS_private_key_id,
-		private_key: process.env.CREDENTIALS_private_key,
-		client_email: process.env.CREDENTIALS_client_email,
-		client_id: process.env.CREDENTIALS_client_id,
-		auth_uri: process.env.CREDENTIALS_auth_uri,
-		token_uri: process.env.CREDENTIALS_token_uri,
-		auth_provider_x509_cert_url: process.env.CREDENTIALS_auth_provider_x509_cert_url,
-		client_x509_cert_url: process.env.CREDENTIALS_client_x509_cert_url
-	}), writeFileError => {
-		if (writeFileError) {
-			errorLog(`Failed to write credentials : ${writeFileError}`);
-			throw writeFileError;
-		} else {
-			infoLog("Google spreadsheets credentials file written successfully !")
+	let credentials = {};
+	for (let credentialProperty of Object.keys(process.env).filter(property => property.startsWith("CREDENTIALS_"))) {
+		credentials[credentialProperty.replace("CREDENTIALS_", "")] = process.env[credentialProperty];
+	}
+	fs.writeFile(
+		"credentials.json",
+		JSON.stringify(credentials),
+		writeFileError => {
+			if (writeFileError) {
+				errorLog(`Failed to write credentials : ${writeFileError}`);
+				throw writeFileError;
+			} else {
+				infoLog("Google spreadsheets credentials file written successfully !")
+			}
 		}
-	});
+	);
 };
 
 const isMee7CommandMessage = message => {
