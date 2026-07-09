1f921e0288056b3449cfccaeb1f4305b86b1e43c
Cubeur-manchot <matthieu.aubert17@gmail.com>
2026-07-10 00:43:59 +0200

    Rewrite data access functions with logs and try catch

--- data.js
+++ data.js
@@ -2,6 +2,8 @@
 
 import {google} from "googleapis";
 
+import logger from "./logger.js";
+
 const auth = new google.auth.GoogleAuth({
 	scopes: [
 		"https://www.googleapis.com/auth/spreadsheets",
@@ -15,21 +17,39 @@ const googleSheetsClient = google.sheets({version: "v4", auth: authClient});
 
 const googleDriveClient = google.drive({version: "v3", auth: authClient});
 
-const loadTableData = async (spreadsheetId, tabName) => 
-	(await googleSheetsClient.spreadsheets.values.get({
-		spreadsheetId: spreadsheetId,
-		range: tabName
-	})).data.values;
+const loadTableData = async (spreadsheetId, tabName) => {
+	try {
+		logger.info(`Loading data from Google Sheets (spreadsheetId: ${spreadsheetId}, tab: ${tabName}).`);
+		const data = (await googleSheetsClient.spreadsheets.values.get({
+			spreadsheetId: spreadsheetId,
+			range: tabName
+		})).data.values;
+		logger.info(`Data successfully loaded from Google Sheets (spreadsheetId: ${spreadsheetId}, tab: ${tabName}).`);
+		return data;
+	} catch (spreadsheetDataLoadingError) {
+		logger.error(`Error while loading data from Google Sheets (spreadsheetId: ${spreadsheetId}, tab: ${tabName}): ${spreadsheetDataLoadingError.stack}`);
+		throw spreadsheetDataLoadingError;
+	}
+};
 
-const loadJsonData = async fileId =>
-	(await googleDriveClient.files.get(
-		{
-			fileId: fileId,
-			alt: "media"
-		},
-		{
-			responseType: "json" // result will be interpreted as object already parsed
-		}
-	)).data;
+const loadJsonData = async fileId => {
+	try {
+		logger.info(`Loading JSON data from Google Drive (fileId: ${fileId}).`);
+		const data = (await googleDriveClient.files.get(
+			{
+				fileId: fileId,
+				alt: "media"
+			},
+			{
+				responseType: "json" // result will be interpreted as object already parsed
+			}
+		)).data;
+		logger.info(`JSON data successfully loaded from Google Drive (fileId: ${fileId}).`);
+		return data;
+	} catch (driveJsonDataLoadingError) {
+		logger.error(`Error while loading JSON data from Google Drive (fileId: ${fileId}): ${driveJsonDataLoadingError.stack}`);
+		throw driveJsonDataLoadingError;
+	}
+};
 
 export {loadTableData, loadJsonData};
